e83b0c38be50669ed27369464130ccb2e3d3893f
lazukakabra <n1h1lum29@gmail.com>
2026-08-27 11:26:19 +0200

    edited path

--- ytdlp.bat
+++ ytdlp.bat
@@ -1,4 +1,4 @@
-py "C:\Users\ks\Videos\ytdlp\yt-dlp.py"
+py "path\to\ytdlp.py"
 :: remove double colon from below if cmd should stay open after running program
 :: useful if script produces error you need to see
 ::@pause
